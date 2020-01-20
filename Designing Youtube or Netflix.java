cddaa2fa481f48d309d6c120eb026a84b38761f1
dheemanthp <dvp499@gmail.com>
2020-01-19 19:28:23 -0800

    Update Designing Youtube or Netflix.java

--- Designing Youtube or Netflix.java
+++ Designing Youtube or Netflix.java
@@ -1,2 +1,13 @@
 
 https://www.educative.io/courses/grokking-the-system-design-interview/xV26VjZ7yMl
+
+Features
+1)Upload/download video
+2)write comments
+3)Rate -> objections
+3)Like/Dislike/subscribe
+4)share
+5)Add to watch list
+
+
+
